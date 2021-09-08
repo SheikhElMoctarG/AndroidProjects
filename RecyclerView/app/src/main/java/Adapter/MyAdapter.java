868b63c8117e42e8cba350910284a30a4e511a4c
Sheikh El Moctar El Shengiti <sheikh.el.moctar@gmail.com>
2021-09-07 20:04:46 -0700

    update project RecyclerView (added onclicklistener)

--- RecyclerView/app/src/main/java/Adapter/MyAdapter.java
+++ RecyclerView/app/src/main/java/Adapter/MyAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -43,7 +44,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
         return listitems.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         private TextView title;
         private TextView description;
         private TextView date;
@@ -53,6 +54,16 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
             title = itemView.findViewById(R.id.title);
             description = itemView.findViewById(R.id.description);
             date = itemView.findViewById(R.id.date);
+            title.setOnClickListener(this);
+            description.setOnClickListener(this);
+            date.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            int position = getAdapterPosition();
+            Listitem item = listitems.get(position);
+            Toast.makeText(context,item.getTitle(),Toast.LENGTH_LONG).show();
         }
     }
 }
--- RecyclerView/app/src/main/java/com/sheikh/recyclerview/MainActivity.java
+++ RecyclerView/app/src/main/java/com/sheikh/recyclerview/MainActivity.java
@@ -25,10 +25,21 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         listitems = new ArrayList<>();
-        for (int a = 1; a<15; a++){
-            Listitem item = new Listitem("Sheikh EL-Moctar","He is a Software Engineer","7/9/2021");
-            listitems.add(item);
-        }
+        // for (int a = 1; a<15; a++){
+        //    Listitem item = new Listitem("Sheikh EL-Moctar","He is a Software Engineer","7/9/2021");
+        //    listitems.add(item);
+        //}
+        Listitem item1 = new Listitem("Sheikh El-Moctar","Software Engineer","2004");
+        Listitem item2 = new Listitem("Jidou Muhammed Lemine","Player Succor","2003");
+        Listitem item3 = new Listitem("Yameen","Web Developer","2002");
+        Listitem item4 = new Listitem("Assmea","Teacher English","2002");
+        Listitem item5 = new Listitem("Muhammed Salem","Docter","2004");
+        listitems.add(item1);
+        listitems.add(item2);
+        listitems.add(item3);
+        listitems.add(item4);
+        listitems.add(item5);
+
         adapter = new MyAdapter(this,listitems);
         recyclerView.setAdapter(adapter);
     }
